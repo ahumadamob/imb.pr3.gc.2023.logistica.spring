46a9539b2cc1f2793f765efb925a49cc7f8e56ba
Ignacio N. Tártaro <86450461+SLNacho@users.noreply.github.com>
2023-11-15 22:22:14 -0300

    Actualizacion nueva para entidad ciudad

--- src/main/java/imb/pr3/estetica/controller/CiudadController.java
+++ src/main/java/imb/pr3/estetica/controller/CiudadController.java
@@ -1,12 +1,12 @@
 package imb.pr3.estetica.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import imb.pr3.estetica.service.ICiudadService;
+import imb.pr3.estetica.util.ResponseUtil;
+import jakarta.validation.ConstraintViolationException;
 import imb.pr3.estetica.entity.Ciudad;
 
 @RestController
@@ -24,100 +26,66 @@ public class CiudadController {
 
 	@Autowired
 	ICiudadService service;
+
+	
+
+
 	
 	@GetMapping("/Ciudad")
-	public ResponseEntity<APIResponse<List<Ciudad>>>obtenerTodasLasCiudads(){
-		APIResponse<List<Ciudad>> response = new APIResponse<List<Ciudad>> (200, null, service.buscarTodos());
-		return ResponseEntity.status(HttpStatus.OK).body(response);
+	public ResponseEntity<APIResponse<List<Ciudad>>> obtenerTodasLasCiudads() {
+	    List<Ciudad> Ciudad = service.buscarTodos();
+	    return Ciudad.isEmpty() ? ResponseUtil.notFound("No hay Ciudades")
+	            : ResponseUtil.success(Ciudad);
 	}
+	
+
+
 	
 	@GetMapping("/Ciudad/{id}")
-	public ResponseEntity<APIResponse<Ciudad>> obtenerCiudadPorId(@PathVariable("id") Integer id) {
-		Ciudad Ciudad = service.buscarPorId(id);
-		if(Ciudad == null){
-			List<String> messages = new ArrayList<>();
-			messages.add("No se encontró la Ciudad con id = " + id.toString());
-			messages.add("Revise el parámetro");
-			APIResponse<Ciudad> response = new APIResponse<Ciudad>(HttpStatus.BAD_REQUEST.value(), messages, null);
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-		}else {
-			APIResponse<Ciudad> response = new APIResponse<Ciudad>(HttpStatus.OK.value(),null,Ciudad);
-			return ResponseEntity.status(HttpStatus.OK).body(response);
-		}
+	public ResponseEntity<APIResponse<Ciudad>> obtenerCiudadPorId(@PathVariable("id") Integer id){
+		return service.existe(id) ?
+				ResponseUtil.success(service.buscarPorId(id)) :
+				ResponseUtil.notFound("No se encontró la Ciudad con el id proporcionado");
 	}
+	
+
 	
 	@PostMapping("/Ciudad")
-	public ResponseEntity<APIResponse<Ciudad>> crearCiudad(@RequestBody Ciudad Ciudad) {
-		if(Ciudad.getId() != null) {
-			Ciudad buscaCiudad = service.buscarPorId(Ciudad.getId());
-			if (buscaCiudad != null) {
-				List<String> messages = new ArrayList<>();
-				messages.add("Ya existe una Ciudad con el ID = " + Ciudad.getId().toString());
-				messages.add("Para actualizar utilice el verbo PUT");
-				APIResponse<Ciudad> response = new APIResponse<Ciudad>(HttpStatus.BAD_REQUEST.value(), messages, null);
-				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-			}
-		}
-		service.guardar(Ciudad);
-		APIResponse<Ciudad> response = new APIResponse<Ciudad>(HttpStatus.CREATED.value(),null,Ciudad);
-		return ResponseEntity.status(HttpStatus.CREATED).body(response);
-		
+    public ResponseEntity<APIResponse<Ciudad>> crearCiudad(@RequestBody Ciudad Ciudad, BindingResult result) {
+        return service.existe(Ciudad.getId()) ? ResponseUtil.badRequest("Ya existe una Ciudad con el identificador proporcionado")
+        		: ResponseUtil.created(service.guardar(Ciudad));
+  	
+    }	
+	
+	
+	
+	@PutMapping("/Ciudad") 
+	public ResponseEntity<APIResponse<Ciudad>> modificarCiudad(@RequestBody Ciudad Ciudad) { 
+		return service.existe(Ciudad.getId()) ? ResponseUtil.success(service.guardar(Ciudad))  
+				: ResponseUtil.badRequest("No existe la Ciudad con el identificador proporcionado");
 	}
 	
-	@PutMapping("/Ciudad")
-	public ResponseEntity<APIResponse<Ciudad>> modificarCiudad(@RequestBody Ciudad Ciudad) {
-		boolean isError;
-		String idStr;
-		if(Ciudad.getId() != null) {
-			Ciudad buscaCiudad = service.buscarPorId(Ciudad.getId());
-			idStr = Ciudad.getId().toString();
-			if (buscaCiudad != null) {
-				//Devolver un OK
-				isError = false;
-			}else {
-				//Devolver un Error
-				isError = true;
-			}
-			
-		}else {
-			idStr = "<No definido>";
-			//Devolver un error
-			isError = true;
-		}
-		
-		if(isError) {
-			//Devolvemos el error
-			List<String> messages = new ArrayList<>();
-			messages.add("No existe una Ciudad con el ID = " + idStr);
-			messages.add("Para crear una nueva utilice el verbo POST");
-			APIResponse<Ciudad> response = new APIResponse<Ciudad>(HttpStatus.BAD_REQUEST.value(), messages, null);
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-		}else {
-			//Devolvemos el OK
-			service.guardar(Ciudad);
-			APIResponse<Ciudad> response = new APIResponse<Ciudad>(HttpStatus.OK.value(),null,Ciudad);
-			return ResponseEntity.status(HttpStatus.OK).body(response);
-		}
-		
-	}
 	
-	@DeleteMapping("/Ciudad/{id}")
-	public ResponseEntity<APIResponse<Ciudad>> eliminarCiudad(@PathVariable("id") Integer id) {
-		Ciudad buscaCiudad = service.buscarPorId(id);
-		if(buscaCiudad == null) {
-			//Error
-			List<String> messages = new ArrayList<>();
-			messages.add("No existe una Ciudad con el ID = " + id.toString());
-			APIResponse<Ciudad> response = new APIResponse<Ciudad>(HttpStatus.BAD_REQUEST.value(),messages,null);
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-		}else {
-			service.eliminar(id);
-			List<String> messages = new ArrayList<>();
-			messages.add("La Ciudad que figura en el cuerpo ha sido eliminada");
-			APIResponse<Ciudad> response = new APIResponse<Ciudad>(HttpStatus.OK.value(),messages,buscaCiudad);
-			return ResponseEntity.status(HttpStatus.OK).body(response);
-			//eliminar
+	 @DeleteMapping("/Ciudad/{id}")
+	 public ResponseEntity<APIResponse<Ciudad>> eliminarCiudad(@PathVariable("id") Integer id) {
+		    if (service.existe(id)) {
+		        service.eliminar(id); // Intenta eliminar la Ciudad
+		        return ResponseUtil.success("Ciudad con ID " + id + " ha sido eliminada correctamente");
+		    } else {
+		        return ResponseUtil.badRequest("No existe la Ciudad con el identificador proporcionado");
+		    }
 		}
-		
-	}
+	
+	
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<APIResponse<Ciudad>> handleException(Exception ex) {    	
+    	return ResponseUtil.badRequest(ex.getMessage());
+    }
+    
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<APIResponse<Ciudad>> handleConstraintViolationException(ConstraintViolationException ex) {
+    	return ResponseUtil.handleConstraintException(ex);
+    }    
+	
+	
 }
--- src/main/java/imb/pr3/estetica/util/ResponseUtil.java
+++ src/main/java/imb/pr3/estetica/util/ResponseUtil.java
@@ -60,4 +60,3 @@ public class ResponseUtil {
         return messages;
     }
 }
-
